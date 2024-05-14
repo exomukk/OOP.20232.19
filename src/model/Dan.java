da6675fc8e8ae79b20eb92925e4fc5359ea49bad
kimduyminh <minhkimfp212@gmail.com>
2024-05-14 23:56:08 +0700

    refactor: refactor based on mvc design pattern

---
+++ src/model/Dan.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Dan {
+    final private int points=1;
+
+    public int getPoints() {
+        return points;
+    }
+}
---
+++ src/model/Quan.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Quan {
+    final private int points=5;
+
+    public int getPoints() {
+        return points;
+    }
+}
