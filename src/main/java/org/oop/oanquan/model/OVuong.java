83200fe50b0ab571da14b270a49ad17f817d4014
aeMinh <aekim.meow@gmail.com>
2024-06-02 13:15:59 +0700

    feat: add gameScene and resources

---
+++ src/main/java/org/oop/oanquan/model/OVuong.java
@@ -0,0 +1,22 @@
+package org.oop.oanquan.model;
+
+public class OVuong{
+    private int soDan;
+    private int soQuan;
+    public OVuong(int soDan, int soQuan){
+        this.soDan = soDan;
+        this.soQuan = soQuan;
+    }
+    public int getSoDan() {
+        return soDan;
+    }
+    public void setSoDan(int soDan) {
+        this.soDan = soDan;
+    }
+    public int getSoQuan() {
+        return soQuan;
+    }
+    public void setSoQuan(int soQuan) {
+        this.soQuan = soQuan;
+    }
+}
